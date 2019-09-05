c0ef7afc577702ea28ea0e172d10b2b60b08fb2c
Duc Anh <ngducanh1223@gmail.com>
2019-09-06 00:40:04 +0700

    Css

--- DisplayMinicart/view/frontend/web/js/view/checkout/smart-loader.js
+++ DisplayMinicart/view/frontend/web/js/view/checkout/smart-loader.js
@@ -4,7 +4,7 @@ define([
     'mage/loader'
 ], function ($, mageTemplate) {
     'use strict';
-    $.widget("smart.loader",$.mage.loader, {
+    $.widget("smart.loader", $.mage.loader, {
         loaderStarted: 0,
         options: {
             icon: '',
@@ -15,15 +15,15 @@ define([
             template:
                 '<div class="loading-mask" data-role="loader"  style="background-color: rgba(187,187,187,.7)">' +
                 '<div class="loader">' +
-                '<img alt="<%- data.texts.imgAlt %>" src="<%- data.icon %>" style="background-color: white">' +
+                '<img alt="<%- data.texts.imgAlt %>" src="<%- data.icon %>"">' +
                 // '<p><%- data.texts.loaderText %></p>' +
-                '<h3 style="position: absolute; width: 100%; text-align: center; bottom: 0; left: 0;">' +
-                'Processing your order....' +
-                '</h3>'+
-                '</div>' +
-                '<div>' +
-                '<h1>hello</h1>'+
+                '<div class="smart-text" style="position: absolute; width: 100%; text-align: center; bottom: 25%; left: 0;">' +
+                // '<div class="smart-text" style="margin: 0 auto; margin-top: 2%; padding-bottom: 10%; padding-top: 10%; text-align: center; width: 30%; background-color: white;">' +
+                '<h3>'+
+                'Processing your order...' +
                 '</div>'+
+                '</h3>' +
+                '</div>' +
                 '</div>'
 
         },
