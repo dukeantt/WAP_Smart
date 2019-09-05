7099f7c63f84901a559bbe121a3d8cd81a08d959
Duc Anh <ngducanh1223@gmail.com>
2019-09-05 22:05:52 +0700

    Add text below loader

--- DisplayMinicart/view/frontend/requirejs-config.js
+++ DisplayMinicart/view/frontend/requirejs-config.js
@@ -2,6 +2,6 @@ var config = {
     map: {
         '*': {
             'Magento_Checkout/js/view/minicart': 'Smart_DisplayMinicart/js/view/minicart-open',
-            'mage/loader' : 'Smart_DisplayMinicart/js/view/checkout/smart-loader'
+            'loader' : 'Smart_DisplayMinicart/js/view/checkout/smart-loader'
         }
     }};
--- DisplayMinicart/view/frontend/web/js/view/checkout/smart-loader.js
+++ DisplayMinicart/view/frontend/web/js/view/checkout/smart-loader.js
@@ -4,7 +4,7 @@ define([
     'mage/loader'
 ], function ($, mageTemplate) {
     'use strict';
-    $.widget("smart.loader",$mage.loader, {
+    $.widget("smart.loader",$.mage.loader, {
         loaderStarted: 0,
         options: {
             icon: '',
@@ -13,10 +13,13 @@ define([
                 imgAlt: $.mage.__('Loading...')
             },
             template:
-                '<div class="loading-mask" data-role="loader">' +
+                '<div class="loading-mask" data-role="loader"  style="background-color: rgba(187,187,187,.7)">' +
                 '<div class="loader">' +
-                '<img alt="<%- data.texts.imgAlt %>" src="<%- data.icon %>">' +
-                '<p><%- data.texts.loaderText %></p>' +
+                '<img alt="<%- data.texts.imgAlt %>" src="<%- data.icon %>" style="background-color: white">' +
+                // '<p><%- data.texts.loaderText %></p>' +
+                '<h3 style="position: absolute; width: 100%; text-align: center; bottom: 0; left: 0;">' +
+                'Processing your order....' +
+                '</h3>'+
                 '</div>' +
                 '<div>' +
                 '<h1>hello</h1>'+
